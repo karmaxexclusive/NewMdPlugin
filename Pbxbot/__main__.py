fd31ac97392b45204069162c1c3f01fabdb87861
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-13 16:55:49 +0530

    Create __main__.py

---
+++ Pbxbot/__main__.py
@@ -0,0 +1,24 @@
+from pyrogram import idle
+
+from Pbxbot import __version__
+from Pbxbot.core import Config, ForcesubSetup, UserSetup, db, Pbxbot
+from Pbxbot.functions.tools import initialize_git
+from Pbxbot.functions.utility import BList, Flood, TGraph
+
+
+async def main():
+    await Pbxbot.startup()
+    await db.connect()
+    await UserSetup()
+    await ForcesubSetup()
+    await Flood.updateFromDB()
+    await BList.updateBlacklists()
+    await TGraph.setup()
+    await initialize_git(Config.PLUGINS_REPO)
+    await Pbxbot.start_message(__version__)
+    await idle()
+
+
+if __name__ == "__main__":
+    Pbxbot.run(main())
+  
