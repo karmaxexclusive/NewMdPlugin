33bef12ba3bb3ac8718faf2838f93fef5d223c16
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-04-09 09:57:09 +0530

    Update __init__.py

--- Pbxbot/core/__init__.py
+++ Pbxbot/core/__init__.py
@@ -1,17 +1,19 @@
 from .clients import Pbxbot
 from .config import ENV, Config, Limits, Symbols
 from .database import db
-from .initializer import ForcesubSetup, UserSetup
+from .initializer import ForcesubSetup, GachaBotsSetup, TemplateSetup, UserSetup
 from .logger import LOGS
 
-__all__ =[
-    "Pbx bot",
+__all__ = [
+    "Pbxbot",
     "ENV",
     "Config",
     "Limits",
     "Symbols",
     "db",
     "ForcesubSetup",
+    "GachaBotsSetup",
+    "TemplateSetup",
     "UserSetup",
-    "LOGS"
+    "LOGS",
 ]
