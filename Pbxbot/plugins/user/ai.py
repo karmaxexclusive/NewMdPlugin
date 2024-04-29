203e84b5a69d6d9243c7f224b20c8c7ca4c6e6db
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-04-29 14:16:09 +0530

    Update ai.py

--- Pbxbot/plugins/user/ai.py
+++ Pbxbot/plugins/user/ai.py
@@ -5,7 +5,7 @@ from . import *
 import requests
 from pyrogram.types import  Message
 from pyrogram.types import InputMediaPhoto
-from MukeshAPI import api
+from BadAPI import api
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
