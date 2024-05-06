e7547c76881eed9e6c601c86294d27e5800a797c
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-05-06 16:58:13 +0530

    Update animation2.py

--- Pbxbot/plugins/user/animation2.py
+++ Pbxbot/plugins/user/animation2.py
@@ -13,3 +13,94 @@ from Pbxbot.bad.constants import MEMES
 from . import *
 
 DEFAULTUSER = "Man"
+
+@on_message("box", allow_stan=True)
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "`boxs...`")
+    deq = deque(list("🟥🟧🟨🟩🟦🟪🟫⬛⬜"))
+    for _ in range(999):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@on_message("rain", allow_stan=True)
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "`Raining.......`")
+    deq = deque(list("🌬☁️🌩🌨🌧🌦🌥⛅🌤"))
+    for _ in range(48):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@on_message("deploy", allow_stan=True)
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 3
+    animation_ttl = range(12)
+    event = await edit_or_reply(event, "`Deploying...`")
+    animation_chars = [
+     "**Heroku Connecting To Latest [Github Build](Badhacker98/PBX_2.0)**",
+            f"**Build started by user** {DEFAULTUSER}",
+            f"**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
+            "**Restarting Heroku Server...**",
+            "**State changed from up to starting**",    
+            "**Stopping all processes with SIGTERM**",
+            "**Process exited with** `status 143`",
+            "**Starting process with command** `python3 -m stdborg`",
+            "**State changed from starting to up**",
+            "__INFO:Pbxẞø†:Logged in as 557667062__",
+            "__INFO:Pbxẞø†:Successfully loaded all plugins__",
+            "**Build Succeeded**",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+
+@on_message("dump", allow_stan=True)
+async def _(message):
+    if event.fwd_from:
+        return
+    try:
+        obj = message.pattern_match.group(1)
+        if len(obj) != 3:
+            raise IndexError
+        inp = " ".join(obj)
+    except IndexError:
+        inp = "🥞 🎂 🍫"
+    event = await edit_or_reply(message, "`droping....`")
+    u, t, g, o, s, n = inp.split(), "🗑", "<(^_^ <)", "(> ^_^)>", "⠀ ", "\n"
+    h = [(u[0], u[1], u[2]), (u[0], u[1], ""), (u[0], "", "")]
+    for something in reversed(
+        [
+            y
+            for y in (
+                [
+                    "".join(x)
+                    for x in (
+                        f + (s, g, s + s * f.count(""), t),
+                        f + (g, s * 2 + s * f.count(""), t),
+                        f[:i] + (o, f[i], s * 2 + s * f.count(""), t),
+                        f[:i] + (s + s * f.count(""), o, f[i], s, t),
+                        f[:i] + (s * 2 + s * f.count(""), o, f[i], t),
+                        f[:i] + (s * 3 + s * f.count(""), o, t),
+                        f[:i] + (s * 3 + s * f.count(""), g, t),
+                    )
+                ]
+                for i, f in enumerate(reversed(h))
+            )
+        ]
+    ):
+        for something_else in something:
+            await asyncio.sleep(0.3)
+            try:
+                await event.edit(something_else)
+            except errors.MessageIdInvalidError:
+                return
