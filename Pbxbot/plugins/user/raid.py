c5cddb9b08364190106eb5f4d11420530bdb4c8a
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-05-13 21:22:10 +0530

    Update raid.py

--- Pbxbot/plugins/user/raid.py
+++ Pbxbot/plugins/user/raid.py
@@ -2,7 +2,7 @@ import asyncio
 from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
-from Pbxbot.bad.sukh import RAID, PBIRAID, OneWord
+from Pbxbot.bad.sukh import RAID, PBIRAID, OneWord, HIRAID
 from . import *
 
 @on_message("raid", allow_stan=True)
@@ -86,5 +86,34 @@ async def raid(x: Client, e: Message):
                 await asyncio.sleep(0.1)
 
       else:
-            await e.reply_text("ᴘʙɪʀᴀɪᴅ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+            await e.reply_text("ᴏɴᴇᴡᴏʀᴅ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+#HIRAID
+
+@on_message("hiraid", allow_stan=True)
+async def raid(x: Client, e: Message):
+      PbxTeam = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(PbxTeam) == 2:
+          ok = await x.get_users(kex[1])
+          counts = int(PbxTeam[0])
+          for _ in range(counts):
+                reply = choice(HIRAID)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(PbxTeam[0])
+          for _ in range(counts):
+                reply = choice(HIRAID)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text("ʜɪʀᴀɪᴅ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
 
